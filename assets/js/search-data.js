0d69e554fdb8daa188b5a996c110b416e02f27b3
kina19131 <65144979+kina19131@users.noreply.github.com>
2025-01-26 16:12:01 +0000

    Deploying to gh-pages from @ kina19131/kina19131.github.io@e89f5b9132130544f96556fe11b9587ead20684c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum-Vitae",
+          description: "A page to explore what I&#39;ve achieved so far and where am currently at!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
