7ed3ca83a14972fc9c98cebf69adf1ab71544245
kina19131 <65144979+kina19131@users.noreply.github.com>
2025-01-28 13:51:28 +0000

    Deploying to gh-pages from @ kina19131/kina19131.github.io@d436bbbae7bd83fc4b171ec443f72cb92811db48 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,12 +59,12 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-nature",
           title: 'Nature',
-          description: "a project with a background image and giscus comments",
+          description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-link",
+          title: 'link',
+          description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-bike",
@@ -79,7 +79,7 @@ ninja.data = [{
               window.location.href = "/projects/5_project/";
             },},{id: "projects-travel",
           title: 'Travel',
-          description: "a project with no image",
+          description: "flying, training, walking everywhere",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-travel",
